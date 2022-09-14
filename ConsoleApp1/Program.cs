ae20ad529b74cdf561f4791800cb8bd54a865ff9
SEM <SEM@DESKTOP-4GKM6HK>
2022-09-14 19:18:01 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿string username;
+string password;
+
+
+for (int i = 0; i < 3; i++)
+{
+	username = Console.ReadLine();
+    password = Console.ReadLine();
+	if (username=="görkem" && password=="123")
+	{
+		Console.WriteLine("Giriş Başarılı");
+		break;
+	}
+	else
+	{
+		if (i==2)
+		{
+			Console.WriteLine("bloke");
+			break;
+		}
+		Console.WriteLine("tekrar dene");
+	}	
+}
